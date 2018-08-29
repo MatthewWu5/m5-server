341b0c2a8f35532a7c0e949230a991daacf86cc3
Matthew5 <jnwu@thoughtworks.com>
2018-08-29 11:44:47 +0800

    Revert "[m5-server][temp]"
    This reverts commit 42c98fd57771e53e1d589e9ebcf7c626d8e18b62.

--- test/index.js
+++
@@ -1,8 +0,0 @@
-var path = require('path')
-var fs = require('fs')
-
-module.exports = function addRouter(router) {
-  router.get('/test', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-  })
-}
