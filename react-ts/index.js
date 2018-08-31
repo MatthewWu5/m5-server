b19ede79b672db4af9a7ca7658723b68307fd8b2
Matthew5 <jnwu@thoughtworks.com>
2018-08-31 10:24:40 +0800

    [m5-server][test]

--- react-ts/index.js
+++ react-ts/index.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var fs = require('fs')
+var users = require('./users')
 
 module.exports = function addRouter(router) {
   router.get('/react-ts/test', function(req, res) {
@@ -7,12 +8,62 @@ module.exports = function addRouter(router) {
     res.sendFile(__dirname + '/package.json')
   })
   router.get('/react-ts/preLogin', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send({token: 'jlfewiwoldfsfd', isLogon: false})
+    res.send({isLogon: isUserLogin(req)})
   })
-  router.get('/react-ts/login', function(req, res) {
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    console.log(req.session)
-    res.send({login: 200})
+  router.post('/react-ts/login', function(req, res) {
+    if(users.isUserExist(req.params.username, req.params.password)){
+      const token = users.getToken(req.params.username, req.params.password)
+      this.addUserSession(req, token)
+      res.send({login: 'success'})
+    }else{
+      res.send({login: 'error'})
+    }
   })
+  router.post('/react-ts/login2', function(req, res) {
+      res.send({username: req.query.username, password: req.query.password})
+  })
+  router.get('/react-ts/contacts', function(req, res) {
+    const All_CONTACTS = [
+      {
+        id: '1',
+        name: {
+          first: 'John',
+          last: 'Doe',
+        },
+        phone: '555',
+        email: 'john@gmail.com',
+      },
+      {
+        id: '2',
+        name: {
+          first: 'Bruce',
+          last: 'Wayne',
+        },
+        phone: '777',
+        email: 'bruce.wayne@gmail.com',
+      },
+      {
+        id: '3',
+        name: {
+          first: 'Bruce',
+          last: 'Wayne',
+        },
+        phone: '777',
+        email: 'bruce.wayne@gmail.com',
+      },
+    ]
+    let result = All_CONTACTS
+    if(req.query && req.query.phone && req.query.phone != 'undefined'){
+      result = result.filter(x=>x.id != '1')
+    }
+    res.send(result)
+  })
+}
+
+function addUserSession(req, token) {
+  req.session.userSession[req.params.username] = token
+}
+
+function isUserLogin(req){
+  return req.session.cookie.user_session && req.session.userSession.find(x=>req.session.userSession[x] == req.session.cookie.user_session)
 }
